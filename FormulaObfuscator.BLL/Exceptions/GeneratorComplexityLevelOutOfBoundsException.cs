3a2dc1d6ba7800c898abd0eb0e0db89a2e963173
tomek <rainbowvme@gmail.com>
2020-10-28 16:26:16 +0100

    Exceptions for new features

---
+++ FormulaObfuscator.BLL/Exceptions/GeneratorComplexityLevelOutOfBoundsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaObfuscator.BLL.Exceptions
+{
+    class GeneratorComplexityLevelOutOfBoundsException : Exception
+    {
+        public GeneratorComplexityLevelOutOfBoundsException()
+        {
+        }
+
+        public GeneratorComplexityLevelOutOfBoundsException(string message)
+            : base(message)
+        {
+        }
+
+        public GeneratorComplexityLevelOutOfBoundsException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
