cf636f15ccd32188ab56ff3514ee8a683db7cb26
tomek <rainbowvme@gmail.com>
2020-10-28 16:21:11 +0100

    MathOperator to generate random operators, for now '+-'

---
+++ FormulaObfuscator.BLL/Models/MathOperator.cs
@@ -0,0 +1,30 @@
+﻿using FormulaObfuscator.BLL.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaObfuscator.BLL.Models
+{
+    public class MathOperator
+    {
+        public char Value { get; set; }
+
+        public MathOperator(char value)
+        {
+            Value = value;
+        }
+
+        public static MathOperator operator !(MathOperator a)
+        {
+            switch (a.Value)
+            {
+                case '+':
+                    return new MathOperator('-');
+                case '-':
+                    return new MathOperator('+');
+
+            }
+            throw new UnhandledOperatorException();
+        }
+    }
+}
