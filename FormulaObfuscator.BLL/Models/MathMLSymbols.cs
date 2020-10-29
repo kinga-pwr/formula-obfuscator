bc79a24597c4a12510dc09a539cd00ef3316d521
tomek <rainbowvme@gmail.com>
2020-10-29 14:26:18 +0100

    Symbol for multiplying

---
+++ FormulaObfuscator.BLL/Models/MathMLSymbols.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaObfuscator.BLL.Models
+{
+    class MathMLSymbols
+    {
+        public string Multiply => "&sdot;";
+    }
+}
