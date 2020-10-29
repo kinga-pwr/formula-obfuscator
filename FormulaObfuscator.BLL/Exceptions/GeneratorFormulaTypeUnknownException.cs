299daeb5f37832d46364a5f504f192ebb8b62229
tomek <rainbowvme@gmail.com>
2020-10-29 14:27:25 +0100

    Exeption name change

---
+++ FormulaObfuscator.BLL/Exceptions/GeneratorFormulaTypeUnknownException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaObfuscator.BLL.Exceptions
+{
+    class GeneratorFormulaTypeUnknownException : Exception
+    {
+        public GeneratorFormulaTypeUnknownException()
+        {
+        }
+
+        public GeneratorFormulaTypeUnknownException(string message)
+            : base(message)
+        {
+        }
+
+        public GeneratorFormulaTypeUnknownException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
