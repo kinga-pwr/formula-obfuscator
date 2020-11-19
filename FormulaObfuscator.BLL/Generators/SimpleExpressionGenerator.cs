d597c9f142db073dc654971bf631044cbb34f0f4
tomek <eskaeldev@gmail.com>
2020-11-19 19:26:28 +0100

    Simple Sum commented

--- FormulaObfuscator.BLL/Generators/SimpleExpressionGenerator.cs
+++ FormulaObfuscator.BLL/Generators/SimpleExpressionGenerator.cs
@@ -65,40 +65,40 @@ namespace FormulaObfuscator.BLL.Generators
         ///</code>
         /// </summary>
         /// <returns></returns>
-        public static XElement Sum()
-        {
-            XElement rootRow = new XElement(MathMLTags.Row);
-            XElement underOver = new XElement(MathMLTags.Underover);
-            #region underover
-            XElement epsilon = new XElement(MathMLTags.Operator, MathMLSymbols.Epsilon);
-            underOver.Add(epsilon);
+        //public static XElement Sum()
+        //{
+        //    XElement rootRow = new XElement(MathMLTags.Row);
+        //    XElement underOver = new XElement(MathMLTags.Underover);
+        //    #region underover
+        //    XElement epsilon = new XElement(MathMLTags.Operator, MathMLSymbols.Epsilon);
+        //    underOver.Add(epsilon);
 
-            XElement underEpsilon = new XElement(MathMLTags.Row);
+        //    XElement underEpsilon = new XElement(MathMLTags.Row);
 
-            string k = Randoms.Char().ToString();
+        //    string k = Randoms.Char().ToString();
 
-            //k
-            underEpsilon.Add(new XElement(MathMLTags.Identifier, k));
-            //=
-            underEpsilon.Add(new XElement(MathMLTags.Operator, MathMLSymbols.Equal));
-            //1
-            underEpsilon.Add(new XElement(MathMLTags.Identifier, MathMLSymbols.One));
-            //r
-            #endregion
-            underOver.Add(underEpsilon);
-            underOver.Add(new XElement(MathMLTags.Identifier, Randoms.CharOrInt()));
-            #region sub
-            XElement expressionRoot = new XElement(MathMLTags.Sub);
-            //c
-            expressionRoot.Add(new XElement(MathMLTags.Identifier, Randoms.Char()));
-            // k
-            XElement subrow = new XElement(MathMLTags.Row);
-            subrow.Add(new XElement(MathMLTags.Identifier, k));
-            expressionRoot.Add(subrow);
-            #endregion
-            rootRow.Add(underOver);
-            rootRow.Add(expressionRoot);
-            return new XElement(MathMLTags.Math, rootRow);
-        }
+        //    //k
+        //    underEpsilon.Add(new XElement(MathMLTags.Identifier, k));
+        //    //=
+        //    underEpsilon.Add(new XElement(MathMLTags.Operator, MathMLSymbols.Equal));
+        //    //1
+        //    underEpsilon.Add(new XElement(MathMLTags.Identifier, MathMLSymbols.One));
+        //    //r
+        //    #endregion
+        //    underOver.Add(underEpsilon);
+        //    underOver.Add(new XElement(MathMLTags.Identifier, Randoms.CharOrInt()));
+        //    #region sub
+        //    XElement expressionRoot = new XElement(MathMLTags.Sub);
+        //    //c
+        //    expressionRoot.Add(new XElement(MathMLTags.Identifier, Randoms.Char()));
+        //    // k
+        //    XElement subrow = new XElement(MathMLTags.Row);
+        //    subrow.Add(new XElement(MathMLTags.Identifier, k));
+        //    expressionRoot.Add(subrow);
+        //    #endregion
+        //    rootRow.Add(underOver);
+        //    rootRow.Add(expressionRoot);
+        //    return new XElement(MathMLTags.Math, rootRow);
+        //}
     }
 }
