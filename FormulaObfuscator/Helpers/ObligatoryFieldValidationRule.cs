83ddaa31ff1656c62f811472eefb6611f8122843
Druciak <ola.druciak@gmail.com>
2020-11-30 20:44:50 +0100

    obligatory letters

---
+++ FormulaObfuscator/Helpers/ObligatoryFieldValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace FormulaObfuscator.Helpers
+{
+    public class ObligatoryFieldValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var result = value != null && (value as string).Length > 0;
+
+            return new ValidationResult(result, "Field obligatory");
+        }
+    }
+}
